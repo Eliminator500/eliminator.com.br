7bf78b7acaff137ef055a5384c366749a90c83e7
daniloalsilva <daniloalsilva@users.noreply.github.com>
2026-04-09 14:39:04 +0000

    deploy: Eliminator500/eliminator@6c72df57e9fa60267ba7e935428ade88944657e8

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -48,7 +48,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Tourer Express",
-        "excerpt":"A Tourer Express é para o piloto que não vê limites no horizonte. Equipada para longas distâncias, ela prioriza o conforto sem sacrificar o desempenho. Itens Indispensáveis &amp; Modificações Instalação de kit de proteção tubular Setup de iluminação auxiliar LED Acessórios Recomendados Bolha Alta (Conforto): Proteção aerodinâmica superior Alforges Rígidos...","categories": [],
+        "excerpt":"A Tourer Express é para o piloto que não vê limites no horizonte. Equipada para longas distâncias, ela prioriza o conforto sem sacrificar o desempenho. Itens Indispensáveis &amp; Modificações Instalação de kit de proteção tubular Setup de iluminação auxiliar LED Suporte de Bauleto Traseiro (Grelha) Acessórios Recomendados Bolha Alta (Conforto):...","categories": [],
         "tags": [],
         "url": "/customizacoes/tourer-express/",
         "teaser": null
